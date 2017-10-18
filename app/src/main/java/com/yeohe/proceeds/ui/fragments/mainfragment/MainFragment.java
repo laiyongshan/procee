6f2dde7518cca4db6bf619e75d75895527be8f62
laiyongshan <850422483@qq.com>
2017-10-18 14:16:59 +0800

    新增下拉刷新，上拉加载功能

--- app/src/main/java/com/yeohe/proceeds/ui/fragments/mainfragment/MainFragment.java
+++ app/src/main/java/com/yeohe/proceeds/ui/fragments/mainfragment/MainFragment.java
@@ -1,13 +1,16 @@
 package com.yeohe.proceeds.ui.fragments.mainfragment;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.ccj.base.base.BaseFragment;
 import com.yeohe.proceeds.R;
@@ -16,13 +19,20 @@ import com.yeohe.proceeds.ui.gesture.GestureActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
+import cn.bingoogolapple.refreshlayout.BGAStickinessRefreshViewHolder;
 
 /**
  * Created by Administrator on 2017/10/16.
  */
 
-public class MainFragment extends BaseFragment {
+public class MainFragment extends BaseFragment implements BGARefreshLayout.BGARefreshLayoutDelegate{
     private View view;
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+
+    @BindView(R.id.rl_normalview_refresh)
+    BGARefreshLayout mRefreshLayout;
 
     @BindView(R.id.btn1)
     Button btn1;
@@ -30,8 +40,22 @@ public class MainFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_main,null);
-        ButterKnife.bind(this,view);
+
+        // 避免多次从xml中加载布局文件
+        if (view == null) {
+            view=inflater.inflate(R.layout.fragment_main,null);
+            ButterKnife.bind(this,view);
+
+            initView();
+            setListener();
+            processLogic(savedInstanceState);
+        } else {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null) {
+                parent.removeView(view);
+            }
+        }
+
         return view;
     }
 
@@ -46,6 +70,66 @@ public class MainFragment extends BaseFragment {
 
     @Override
     public void initView() {
+//        mRefreshLayout = (BGARefreshLayout) view.findViewById(R.id.rl_normalview_refresh);
 
+    }
+
+    protected void setListener() {
+        mRefreshLayout.setDelegate(this);
+
+    }
+
+    protected void processLogic(Bundle savedInstanceState) {
+        BGAStickinessRefreshViewHolder stickinessRefreshViewHolder = new BGAStickinessRefreshViewHolder(getActivity(), true);
+        stickinessRefreshViewHolder.setStickinessColor(R.color.colorPrimary);
+        stickinessRefreshViewHolder.setRotateImage(R.mipmap.bga_refresh_stickiness);
+        mRefreshLayout.setRefreshViewHolder(stickinessRefreshViewHolder);
+
+//        mRefreshLayout.setCustomHeaderView(DataEngine.getCustomHeaderView(getActivity()), false);
+    }
+
+    public static final int LOADING_DURATION = 2000;
+    @Override
+    public void onBGARefreshLayoutBeginRefreshing(BGARefreshLayout refreshLayout) {
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Thread.sleep(LOADING_DURATION);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                mRefreshLayout.endRefreshing();
+            }
+        }.execute();
+    }
+
+    @Override
+    public boolean onBGARefreshLayoutBeginLoadingMore(BGARefreshLayout refreshLayout) {
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Thread.sleep(LOADING_DURATION);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                mRefreshLayout.endLoadingMore();
+                Log.i(TAG, "上拉加载更多完成");
+            }
+        }.execute();
+        return true;
     }
 }
--- app/src/main/java/com/yeohe/proceeds/ui/fragments/sharefragment/ShareFragment.java
+++ app/src/main/java/com/yeohe/proceeds/ui/fragments/sharefragment/ShareFragment.java
@@ -6,9 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.ccj.base.base.BaseFragment;
 import com.yeohe.proceeds.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/10/17.
@@ -17,12 +22,15 @@ import com.yeohe.proceeds.R;
 public class ShareFragment extends BaseFragment {
     private View view;
 
+    @BindView(R.id.btn)
+    Button btn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view=inflater.inflate(R.layout.fragment_share,null);
-
+        ButterKnife.bind(this,view);
 
         return view;
     }
@@ -30,5 +38,15 @@ public class ShareFragment extends BaseFragment {
     @Override
     public void initView() {
 
+    }
+
+
+    @OnClick({R.id.btn})
+    public void OnClick(View view){
+        switch (view.getId()){
+            case R.id.btn:
+
+                break;
+        }
     }
 }

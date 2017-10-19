bc5829aa9abd0d89c05543df5432b65607144c9f
laiyongshan <850422483@qq.com>
2017-10-19 18:28:09 +0800

    新增日期选择

--- app/src/main/java/com/yeohe/proceeds/ui/fragments/sharefragment/ShareFragment.java
+++ app/src/main/java/com/yeohe/proceeds/ui/fragments/sharefragment/ShareFragment.java
@@ -9,10 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.bigkoo.pickerview.TimePickerView;
 import com.ccj.base.base.BaseFragment;
 import com.yeohe.proceeds.R;
 import com.yeohe.proceeds.ui.gesture.GestureActivity;
 import com.yeohe.proceeds.ui.other.PasswordInputActivity;
+import com.yeohe.proceeds.utils.ToastUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,7 +37,10 @@ public class ShareFragment extends BaseFragment {
     @BindView(R.id.btn2)
     Button btn2;
 
-    @Nullable
+    @BindView(R.id.btn3)
+    Button btn3;
+
+  @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -47,7 +56,7 @@ public class ShareFragment extends BaseFragment {
     }
 
 
-    @OnClick({R.id.btn1,R.id.btn2})
+    @OnClick({R.id.btn1,R.id.btn2,R.id.btn3})
     public void OnClick(View view){
         switch (view.getId()){
             case R.id.btn1:
@@ -57,6 +66,25 @@ public class ShareFragment extends BaseFragment {
             case R.id.btn2:
                 startActivity(new Intent(getActivity(), PasswordInputActivity.class));
                 break;
+
+            case R.id.btn3:
+                //时间选择器
+                TimePickerView pvTime = new TimePickerView.Builder(getActivity(), new TimePickerView.OnTimeSelectListener() {
+                    @Override
+                    public void onTimeSelect(Date date, View v) {//选中事件回调
+                        ToastUtil.showMessage(getContext(),getTime(date));
+                    }
+                }).setType(new boolean[]{true, true, true, false, false, false})
+                        .build();
+                pvTime.setDate(Calendar.getInstance());//注：根据需求来决定是否使用该方法（一般是精确到秒的情况），此项可以在弹出选择器的时候重新设置当前时间，避免在初始化之后由于时间已经设定，导致选中时间与当前时间不匹配的问题。
+                pvTime.show();
+                break;
         }
+    }
+
+
+    private String getTime(Date date) {//可根据需要自行截取数据显示
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(date);
     }
 }
